edbf6eef37cb17cd847edf5dbdee1ada7a85b4e1
S2Ruby <jwjang1102@naver.com>
2025-03-30 03:45:25 +0900

    2025 창업동아리 slamfinde

---
+++ src/main/java/com/example/slamfinder/SlamfinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.slamfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlamfinderApplication.class, args);
+	}
+
+}
